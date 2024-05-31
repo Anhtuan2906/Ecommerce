cbbaa111df19be72b04eda4da05d3e666389f6da
datn2107 <datn2107@gmail.com>
2024-05-31 12:55:34 +0700

    Last hope success

--- recommendations/reranking.py
+++ recommendations/reranking.py
@@ -1,9 +1,18 @@
 import numpy as np
+import cvxpy as cp
+from tqdm import trange
 from mip import Model, xsum, maximize
+from ortools.linear_solver import pywraplp\
 
 
+def normalize(S: np.ndarray, base_val: float = 0) -> np.ndarray:
+    S = S - np.min(S) + base_val
+    S = S / (np.max(S) -  np.min(S))
 
-def optimize(self, S: np.ndarray, k: int = 30, epsilon: int = 15) -> np.ndarray:
+    return S
+
+
+def optimize(S: np.ndarray, k: int = 30, epsilon: int = 15) -> np.ndarray:
     n_user = S.shape[0]
     n_item = S.shape[1]
 
@@ -31,3 +40,144 @@ def optimize(self, S: np.ndarray, k: int = 30, epsilon: int = 15) -> np.ndarray:
         [[W[i][j].x for j in range(S.shape[1])] for i in range(S.shape[0])]
     )
     return W_np
+
+
+def optimizeORTools(S: np.ndarray, k: int = 30, epsilon: int = 5) -> np.ndarray:
+    solver = pywraplp.Solver.CreateSolver("PDLP")
+
+    if solver is None:
+        return
+
+    n_users, n_items = S.shape[:2]
+
+    x = {}
+    for i in range(n_users):
+        for j in range(n_items):
+            x[i, j] = solver.BoolVar(f"x_{i}_{j}")
+
+    for i in range(n_users):
+        solver.Add(sum(x[i, j] for j in range(n_items)) == k)
+
+    for j in range(n_items):
+        solver.Add(sum(x[i, j] for i in range(n_users)) >= epsilon)
+
+    objective = solver.Objective()
+    for i in range(n_users):
+        for j in range(n_items):
+            objective.SetCoefficient(x[i, j], int(S[i, j] * 1000))
+    objective.SetMaximization()
+
+    solver.Solve()
+
+    W = np.array(
+        [[x[i, j].solution_value() for j in range(n_items)] for i in range(n_users)]
+    )
+    return W
+
+
+def get_item_provider_mapper(S: np.ndarray, p=0.05):
+    items_count = np.sum(S, axis=0)
+    items_id_sorted = np.argsort(items_count)
+
+    item_interval = int(len(items_id_sorted) * p)
+    item2provider = np.zeros(len(items_id_sorted), dtype=np.int32)
+    for i, s in enumerate(range(0, len(items_id_sorted), item_interval)):
+        item2provider[items_id_sorted[s : s + item_interval]] = i
+    return item2provider
+
+
+class UMMFReRanking():
+    def relabel_provider(self, interactions, preference_scores=None, p=0.05):
+        if preference_scores is not None:
+            item2provider = get_item_provider_mapper(preference_scores, p)
+            interactions[:, 3] = item2provider[interactions[:, 1]]
+
+        interactions[:, 3] = np.unique(interactions[:, 3], return_inverse=True)[1]
+        return interactions
+
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def cpu_layer(self, ordered_tilde_dual, rho, lambd):
+        m = len(rho)
+        answer = cp.Variable(m)
+        objective = cp.Minimize(
+            cp.sum_squares(
+                cp.multiply(rho, answer) - cp.multiply(rho, ordered_tilde_dual)
+            )
+        )
+
+        constraints = []
+        for i in range(1, m + 1):
+            constraints += [cp.sum(cp.multiply(rho[:i], answer[:i])) >= -lambd]
+        prob = cp.Problem(objective, constraints)
+        prob.solve()
+
+        return answer.value
+
+    def compute_next_dual(self, eta, rho, dual, gradient, lambd):
+        tilde_dual = dual - eta * gradient / rho / rho
+        order = np.argsort(tilde_dual * rho)
+        ordered_tilde_dual = tilde_dual[order]
+        ordered_next_dual = self.cpu_layer(ordered_tilde_dual, rho[order], lambd)
+        return ordered_next_dual[order.argsort()]
+
+    def optimize(self, S: np.ndarray, k: int = 30, p: float = 0.05, *args, **kargs) -> np.ndarray:
+        lambd = kargs.get("lambd", 0.1)
+        alpha = kargs.get("alpha", 0.1)
+        eta = kargs.get("eta", 1e-3)
+
+        n_users, n_items = S.shape[:2]
+        item2provider = get_item_provider_mapper(S, p)
+
+        n_providers = len(np.unique(item2provider))
+        providers_cnt = np.unique(item2provider, return_counts=True)[1]
+
+        rho = (1 + 1 / n_providers) * providers_cnt / np.sum(providers_cnt)
+        UI_matrix = S[np.arange(n_users)]
+
+        # normalize user-item perference score to [0,1]
+        UI_matrix = self.sigmoid(UI_matrix)
+
+        # set item-provider matrix
+        A = np.zeros((n_items, n_providers))
+        iid2pid = []
+        for i in range(n_items):
+            iid2pid.append(item2provider[i])
+            A[i, item2provider[i]] = 1
+
+
+        result_x = []
+
+        mu_t = np.zeros(n_providers)
+        B_t = n_users * k * rho
+        sum_dual = 0
+        eta_t = eta / np.sqrt(n_users)
+        gradient_cusum = np.zeros(n_providers)
+        for t in trange(n_users):
+            x_title = UI_matrix[t, :] - np.matmul(A, mu_t)
+            mask = np.matmul(A, (B_t > 0).astype(np.float32))
+
+            mask = (1.0 - mask) * -10000.0
+            x = np.argsort(x_title + mask, axis=-1)[::-1]
+            x_allocation = x[:k]
+            re_allocation = np.argsort(UI_matrix[t, x_allocation])[::-1]
+            x_allocation = x_allocation[re_allocation]
+            result_x.append(x_allocation)
+
+            B_t = B_t - np.sum(A[x_allocation], axis=0, keepdims=False)
+            gradient = -np.mean(A[x_allocation], axis=0, keepdims=False) + B_t / (
+                n_users * k
+            )
+
+            gradient = alpha * gradient + (1 - alpha) * gradient_cusum
+            gradient_cusum = gradient
+            mu_t = self.compute_next_dual(eta_t, rho, mu_t, gradient, lambd)
+            sum_dual += mu_t
+
+
+        W = np.zeros((n_users, n_items))
+        for i in range(n_users):
+            W[i, result_x[i]] = 1
+
+        return W
--- recommendations/views.py
+++ recommendations/views.py
@@ -3,76 +3,125 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 from store.models import Product, Prediction, OriginalPrediction, Interaction
+from customers.models import User
 from recommendations.models import ProductFeaturesVector, UserFeaturesVector
-from recommendations.reranking import optimize
+from recommendations.reranking import optimize, normalize, optimizeORTools, UMMFReRanking
+
+
+def convert_preference_matrix_decision_matrix(S, top_k=10):
+    S_idx = np.argsort(S, axis=1)
+    B = np.zeros_like(S)
+    for i in range(S.shape[0]):
+        B[i, S_idx[i, -top_k:]] = 1
+    return B
+
+
+def convert_predictons_products_list(predictions):
+    products = {}
+
+    for prediction in predictions:
+        product_id = prediction.product.id
+        if product_id not in products:
+            products[product_id] = 0
+        products[product_id] += 1
+
+    return [(Product.objects.get(id=product_id), count) for product_id, count in products.items()]
 
 
 @login_required
 def index(request):
+    message = None
+    is_error = False
     if request.method == 'POST':
-        k = int(request.POST.get('no_recomend_item', 10))
-        epsilon = float(request.POST.get('max_exposure', 0.1))
+        k = int(request.POST.get('k', 10))
+        p = float(request.POST.get('p', 10)) / 100
 
         # Retrieve all user feature vectors
-        # user_feature_vectors = UserFeaturesVector.objects.all()
-        # if not user_feature_vectors.exists():
-        #     return render(request, 'recommendations/index.html', {'error': 'No user feature vectors found'})
+        user_feature_vectors = UserFeaturesVector.objects.all()
+        if not user_feature_vectors.exists():
+            return render(request, 'recommendations/index.html', {'error': 'No user feature vectors found'})
 
-        # user_ids = [ufv.user_id.id for ufv in user_feature_vectors]
-        # user_matrix = np.array([np.fromstring(ufv.feature_vector.strip('[]'), sep=',') for ufv in user_feature_vectors])
+        user_ids = [ufv.user_id.id for ufv in user_feature_vectors]
+        user_matrix = np.array([np.fromstring(ufv.feature_vector.strip('[]'), sep=',') for ufv in user_feature_vectors])
 
-        # # Retrieve all product feature vectors
-        # product_feature_vectors = ProductFeaturesVector.objects.all()
-        # if not product_feature_vectors.exists():
-        #     return render(request, 'recommendations/index.html', {'error': 'No product feature vectors found'})
 
-        # product_ids = [pfv.product_id.id for pfv in product_feature_vectors]
-        # product_matrix = np.array([np.fromstring(pfv.feature_vector.strip('[]'), sep=',') for pfv in product_feature_vectors])
+        # Retrieve all product feature vectors
+        product_feature_vectors = ProductFeaturesVector.objects.all()
+        if not product_feature_vectors.exists():
+            return render(request, 'recommendations/index.html', {'error': 'No product feature vectors found'})
 
-        # # Debugging prints
-        # print(f"User Matrix Shape: {user_matrix.shape}")
-        # print(f"Product Matrix Shape: {product_matrix.shape}")
+        product_ids = [pfv.product_id.id for pfv in product_feature_vectors]
+        product_matrix = np.array([np.fromstring(pfv.feature_vector.strip('[]'), sep=',') for pfv in product_feature_vectors])
 
-        # # Ensure the matrices can be multiplied
-        # if user_matrix.shape[1] != product_matrix.shape[1]:
-        #     raise ValueError("User and product feature vectors must have the same length")
 
-        # # Multiply the user matrix by the product matrix
-        # interaction_matrix = np.dot(user_matrix, product_matrix.T)
-        # optimized_matrix = optimize(interaction_matrix, k=k, epsilon=epsilon)
+        # Debugging prints
+        print(f"User Matrix Shape: {user_matrix.shape}")
+        print(f"Product Matrix Shape: {product_matrix.shape}")
 
-        # # Debugging prints
-        # print(f"Interaction Matrix Shape: {optimized_matrix.shape}")
 
-        # # Dummy context for demonstration purposes
-        # context = {
-        #     'pro
-        #     'user_ids': user_ids,
-        #     'product_ids': product_ids,
-        # }
+        # Ensure the matrices can be multiplied
+        if user_matrix.shape[1] != product_matrix.shape[1]:
+            raise ValueError("User and product feature vectors must have the same length")
 
-        # # Save prediction results (example with dummy predictions)
-        # Prediction.objects.all().delete()
-        # for user_id, user_vector in zip(user_ids, optimized_matrix):
-        #     for product_id, prediction_value in zip(product_ids, user_vector):
-        #         if prediction_value == 1:
-        #             Prediction.objects.create(user_id=user_id, item_id=product_id, prediction_value=prediction_value)
-    else:
-        pass
+        interaction_matrix = np.dot(user_matrix, product_matrix.T)
+        interaction_matrix = normalize(interaction_matrix)
+        print(interaction_matrix)
 
-    context = {}
+        # Save prediction results (original)
+        decision_matrix = convert_preference_matrix_decision_matrix(interaction_matrix, top_k=k)
+        OriginalPrediction.objects.all().delete()
+        for user_id, user_vector in zip(user_ids, decision_matrix):
+            for product_id, prediction_value in zip(product_ids, user_vector):
+                if prediction_value == 1:
+                    OriginalPrediction.objects.create(user=User.objects.get(id=user_id), product=Product.objects.get(id=product_id), prediction_value=prediction_value)
+
+
+        # Save prediction results (reranking)
+        if p > 0:
+            last_hope = UMMFReRanking()
+            try:
+                reranked_decision_matrix = last_hope.optimize(interaction_matrix, k=k, p=p)
+            except Exception as e:
+                reranked_decision_matrix = decision_matrix
+                is_error = True
+                message = str(e)
+            if np.sum(reranked_decision_matrix) == 0:
+                reranked_decision_matrix = decision_matrix
+                is_error = True
+                message = "No solution"
+        else:
+            reranked_decision_matrix = decision_matrix
+        Prediction.objects.all().delete()
+        for user_id, user_vector in zip(user_ids, reranked_decision_matrix):
+            for product_id, prediction_value in zip(product_ids, user_vector):
+                if prediction_value == 1:
+                    Prediction.objects.create(user=User.objects.get(id=user_id), product=Product.objects.get(id=product_id), prediction_value=prediction_value)
+
+        # Debugging prints
+        print(f"Interaction Matrix Shape: {interaction_matrix.shape}")
+        print(f"Decision Matrix Shape: {decision_matrix.shape}")
+        print(f"Reranked Decision Matrix Shape: {reranked_decision_matrix.shape}")
+
+    context = {
+        "is_error": is_error,
+        "message": message,
+        "recommended_products_1": convert_predictons_products_list(OriginalPrediction.objects.all()),
+        "recommended_products_2": convert_predictons_products_list(Prediction.objects.all())
+    }
+
     return render(request, 'recommendations/index.html', context)
 
 
 @login_required
 def interaction_history(request):
     user_interactions = Interaction.objects.filter(user=request.user)
+    user_original_predictions = OriginalPrediction.objects.filter(user=request.user)
     user_predictions = Prediction.objects.filter(user=request.user)
-    recommended_products = [(pred.item_id, pred.prediction_value) for pred in user_predictions]
 
     context = {
-        'user_interactions': user_interactions,
-        'user_predictions': user_predictions,
-        'recommended_products': recommended_products
+        "user_interactions": user_interactions,
+        "recommended_products_1": convert_predictons_products_list(user_original_predictions),
+        "recommended_products_2": convert_predictons_products_list(user_predictions)
     }
+
     return render(request, 'recommendations/interaction_history.html', context)
